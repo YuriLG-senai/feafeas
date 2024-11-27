20506c4221b95bf979b849836b9c25c2d14695f6
Aluno <yuril3300@gmail.com>
2024-11-26 21:55:14 -0300

    massagem

---
+++ Task/Models/ModelTask.cs
@@ -0,0 +1,9 @@
+﻿namespace Task.Models
+{
+    public class ModelTask
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int id { get; set; }
+    }
+}
